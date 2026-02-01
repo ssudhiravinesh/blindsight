011b5fb75c290c9a68cc4884ee3f9e3c89da8a4e
SSudhirAvinesh <175300372+ssudhiravinesh@users.noreply.github.com>
2026-02-01 13:42:17 +0530

    drafting detector logic

---
+++ content.js
@@ -0,0 +1,25 @@
+const SIGNUP_BUTTON_KEYWORDS = [
+    'sign up',
+    'signup',
+    'register',
+    'create account',
+    'create an account',
+    'join',
+    'join now',
+    'get started',
+    'start free',
+    'try free',
+    'sign in', 
+];
+
+const TERMS_CHECKBOX_KEYWORDS = [
+    'i agree',
+    'i accept',
+    'terms of service',
+    'terms and conditions',
+    'privacy policy',
+    'agree to',
+    'accept the',
+];
+
+function matchesKeywords(text, keywords) {
